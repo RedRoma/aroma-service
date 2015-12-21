6578b861b136a313f87c5ef2573ce01a7f0afe03
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-20 23:16:04 -0800

    GetMyApplicationsOperation updates + Services are now generated more organically, with a data model closer to the real deal.

--- src/main/java/tech/aroma/banana/service/operations/GetMyApplicationsOperation.java
+++ src/main/java/tech/aroma/banana/service/operations/GetMyApplicationsOperation.java
@@ -1,38 +1,44 @@
-/*
- * Copyright 2015 Aroma Tech.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ /*
+  * Copyright 2015 Aroma Tech.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 package tech.aroma.banana.service.operations;
 
+import java.time.Instant;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.aroma.banana.thrift.Application;
+import tech.aroma.banana.thrift.Human;
+import tech.aroma.banana.thrift.ProgrammingLanguage;
 import tech.aroma.banana.thrift.service.GetMyApplicationsRequest;
 import tech.aroma.banana.thrift.service.GetMyApplicationsResponse;
 import tech.sirwellington.alchemy.generator.AlchemyGenerator;
-import tech.sirwellington.alchemy.generator.ObjectGenerators;
+import tech.sirwellington.alchemy.generator.EnumGenerators;
 import tech.sirwellington.alchemy.generator.PeopleGenerators;
+import tech.sirwellington.alchemy.generator.TimeGenerators;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
 import static tech.aroma.banana.service.BananaAssertions.checkNotNull;
 import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
 import static tech.sirwellington.alchemy.generator.CollectionGenerators.listOf;
 import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
+import static tech.sirwellington.alchemy.generator.NumberGenerators.positiveLongs;
+import static tech.sirwellington.alchemy.generator.StringGenerators.alphanumericString;
+import static tech.sirwellington.alchemy.generator.StringGenerators.uuids;
 
 /**
  *
@@ -40,37 +46,52 @@ import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
  */
 final class GetMyApplicationsOperation implements ThriftOperation<GetMyApplicationsRequest, GetMyApplicationsResponse>
 {
-
+    
     private final static Logger LOG = LoggerFactory.getLogger(GetMyApplicationsOperation.class);
-
+    
     @Override
     public GetMyApplicationsResponse process(GetMyApplicationsRequest request) throws TException
     {
         checkNotNull(request);
         
         LOG.debug("Received request to GetMyApplications {}", request);
-
+        
         GetMyApplicationsResponse response = new GetMyApplicationsResponse();
-
+        
         int count = one(integers(0, 40));
-
-        if (count == 0)
-        {
-            return response;
-        }
-
-        AlchemyGenerator<Application> serviceGenerator = ObjectGenerators.pojos(Application.class);
-        AlchemyGenerator<String> nameGenerator = PeopleGenerators.names();
-
-        List<Application> fakeApplications = listOf(serviceGenerator, count).stream()
-            .map(s -> s.setName(nameGenerator.get()))
-            .collect(Collectors.toList());
-
-        LOG.info("Returning {} Applications for {}", fakeApplications.size(), request);
+        List<Application> fakeApplications = listOf(applications, count);
         response.setApplications(fakeApplications);
-
+        
+        LOG.info("Returning {} Applications for {}", fakeApplications.size(), request);
+        
         return response;
-
+        
     }
-
+    
+    private final AlchemyGenerator<String> names = PeopleGenerators.names();
+    private final AlchemyGenerator<Instant> times = TimeGenerators.pastInstants();
+    private final AlchemyGenerator<Human> people = () ->
+    {
+        return new Human()
+            .setName(names.get())
+            .setEmail(one(PeopleGenerators.emails()))
+            .setUsername(one(alphanumericString()));
+    };
+    
+    private final AlchemyGenerator<ProgrammingLanguage> languages = EnumGenerators.enumValueOf(ProgrammingLanguage.class);
+    private final AlchemyGenerator<Application> applications = () ->
+    {
+        int numberOfOwners = one(integers(1, 4));
+        
+        return new Application()
+            .setId(one(uuids))
+            .setName(names.get())
+            .setProgrammingLanguage(languages.get())
+            .setSubscribers(listOf(people))
+            .setOwners(listOf(people, numberOfOwners))
+            .setTotalMessagesSent(one(positiveLongs()))
+            .setTimeOfProvisioning(times.get().toEpochMilli());
+        
+    };
+    
 }
