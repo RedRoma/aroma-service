fe580a0baaa46687f85d6a27e51fb04448f71579
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-28 19:33:58 -0800

    Adding GetFullMessageOperation

---
+++ src/main/java/tech/aroma/banana/service/operations/GetFullMessageOperation.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.aroma.banana.service.operations;
+
+
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.banana.thrift.service.GetFullMessageRequest;
+import tech.aroma.banana.thrift.service.GetFullMessageResponse;
+import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
+
+import static tech.aroma.banana.service.BananaAssertions.checkNotNull;
+import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
+
+/**
+ *
+ * @author SirWellington
+ */
+final class GetFullMessageOperation implements ThriftOperation<GetFullMessageRequest, GetFullMessageResponse>
+{
+    private final static Logger LOG = LoggerFactory.getLogger(GetFullMessageOperation.class);
+
+    @Override
+    public GetFullMessageResponse process(GetFullMessageRequest request) throws TException
+    {
+        checkNotNull(request);
+        
+        GetFullMessageResponse response = pojos(GetFullMessageResponse.class).get();
+        
+        return response;
+    }
+
+}
