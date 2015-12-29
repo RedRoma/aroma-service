5b6af9c69611f66235b6afd7ff1b2bf8cf3ca9a4
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-28 19:46:18 -0800

    Adding missing Operations

---
+++ src/main/java/tech/aroma/banana/service/operations/RenegerateApplicationTokenOperation.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.service.operations;
+
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.banana.thrift.service.RegenerateApplicationTokenRequest;
+import tech.aroma.banana.thrift.service.RegenerateApplicationTokenResponse;
+import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
+
+import static tech.aroma.banana.service.BananaAssertions.checkNotNull;
+import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
+
+/**
+ *
+ * @author SirWellington
+ */
+final class RenegerateApplicationTokenOperation implements ThriftOperation<RegenerateApplicationTokenRequest, RegenerateApplicationTokenResponse>
+{
+
+    private final static Logger LOG = LoggerFactory.getLogger(RenegerateApplicationTokenOperation.class);
+
+    @Override
+    public RegenerateApplicationTokenResponse process(RegenerateApplicationTokenRequest request) throws TException
+    {
+        checkNotNull(request);
+
+        return pojos(RegenerateApplicationTokenResponse.class).get();
+    }
+
+}
