de563740c067888d9431eb188479e447ffa1328c
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-04 22:17:50 -0800

    Updating GetDashboardOperationTest

--- src/test/java/tech/aroma/banana/service/operations/GetDashboardOperationTest.java
+++ src/test/java/tech/aroma/banana/service/operations/GetDashboardOperationTest.java
@@ -34,7 +34,7 @@ import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThr
  *
  * @author SirWellington
  */
-@Repeat(10)
+@Repeat(50)
 @RunWith(AlchemyTestRunner.class)
 public class GetDashboardOperationTest 
 {
