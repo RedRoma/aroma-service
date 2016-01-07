41e0f022200477f5b0f25a7d0f5caee112e56797
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-07 13:11:55 -0800

    Updating GetMyApplicationsOperation + Delegating generation of mock data

--- src/main/java/tech/aroma/banana/service/operations/GetMyApplicationsOperation.java
+++ src/main/java/tech/aroma/banana/service/operations/GetMyApplicationsOperation.java
@@ -16,30 +16,20 @@
 
 package tech.aroma.banana.service.operations;
 
-import java.time.Instant;
 import java.util.List;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.aroma.banana.thrift.Application;
-import tech.aroma.banana.thrift.ProgrammingLanguage;
-import tech.aroma.banana.thrift.User;
 import tech.aroma.banana.thrift.service.GetMyApplicationsRequest;
 import tech.aroma.banana.thrift.service.GetMyApplicationsResponse;
-import tech.sirwellington.alchemy.generator.AlchemyGenerator;
-import tech.sirwellington.alchemy.generator.EnumGenerators;
-import tech.sirwellington.alchemy.generator.PeopleGenerators;
-import tech.sirwellington.alchemy.generator.TimeGenerators;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
-import static sir.wellington.alchemy.collections.sets.Sets.toSet;
 import static tech.aroma.banana.service.BananaAssertions.checkNotNull;
+import static tech.aroma.banana.service.BananaGenerators.applications;
 import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
 import static tech.sirwellington.alchemy.generator.CollectionGenerators.listOf;
 import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
-import static tech.sirwellington.alchemy.generator.NumberGenerators.positiveLongs;
-import static tech.sirwellington.alchemy.generator.StringGenerators.alphanumericString;
-import static tech.sirwellington.alchemy.generator.StringGenerators.uuids;
 
 /**
  *
@@ -60,7 +50,7 @@ final class GetMyApplicationsOperation implements ThriftOperation<GetMyApplicati
         GetMyApplicationsResponse response = new GetMyApplicationsResponse();
         
         int count = one(integers(0, 40));
-        List<Application> fakeApplications = listOf(applications, count);
+        List<Application> fakeApplications = listOf(applications(), count);
         response.setApplications(fakeApplications);
         
         LOG.info("Returning {} Applications for {}", fakeApplications.size(), request);
@@ -69,30 +59,6 @@ final class GetMyApplicationsOperation implements ThriftOperation<GetMyApplicati
         
     }
     
-    private final AlchemyGenerator<String> names = PeopleGenerators.names();
-    private final AlchemyGenerator<Instant> times = TimeGenerators.pastInstants();
-    private final AlchemyGenerator<User> users = () ->
-    {
-        return new User()
-            .setName(names.get())
-            .setEmail(one(PeopleGenerators.emails()))
-            .setUserId(one(alphanumericString()));
-    };
     
-    private final AlchemyGenerator<ProgrammingLanguage> languages = EnumGenerators.enumValueOf(ProgrammingLanguage.class);
-    private final AlchemyGenerator<Application> applications = () ->
-    {
-        int numberOfOwners = one(integers(1, 4));
-        
-        return new Application()
-            .setId(one(uuids))
-            .setName(names.get())
-            .setProgrammingLanguage(languages.get())
-            .setSubscribers(toSet(listOf(users)))
-            .setOwners(toSet(listOf(users, numberOfOwners)))
-            .setTotalMessagesSent(one(positiveLongs()))
-            .setTimeOfProvisioning(times.get().toEpochMilli());
-        
-    };
     
 }
