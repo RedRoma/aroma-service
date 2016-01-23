09b6407eb6c4839ca4690a7494b28e2a8cd554d7
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-23 10:42:28 -0800

    SignUpOperation: Adding legit implementation

--- src/main/java/tech/aroma/banana/service/operations/SignUpOperation.java
+++ src/main/java/tech/aroma/banana/service/operations/SignUpOperation.java
@@ -14,19 +14,34 @@
  * limitations under the License.
  */
 
- 
 package tech.aroma.banana.service.operations;
 
-
+import java.util.UUID;
+import java.util.function.Function;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sir.wellington.alchemy.collections.sets.Sets;
+import tech.aroma.banana.data.UserRepository;
+import tech.aroma.banana.thrift.User;
+import tech.aroma.banana.thrift.authentication.AuthenticationToken;
+import tech.aroma.banana.thrift.authentication.TokenType;
+import tech.aroma.banana.thrift.authentication.UserToken;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenRequest;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenResponse;
+import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.banana.thrift.exceptions.OperationFailedException;
 import tech.aroma.banana.thrift.service.SignUpRequest;
 import tech.aroma.banana.thrift.service.SignUpResponse;
+import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
-import static tech.aroma.banana.service.BananaAssertions.checkNotNull;
-import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
+import static tech.aroma.banana.data.assertions.AuthenticationAssertions.completeToken;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
+import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.validUUID;
 
 /**
  *
@@ -34,21 +49,121 @@ import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
  */
 final class SignUpOperation implements ThriftOperation<SignUpRequest, SignUpResponse>
 {
+    
     private final static Logger LOG = LoggerFactory.getLogger(SignUpOperation.class);
+    
+    private final UserRepository userRepo;
+    private final AuthenticationService.Iface authenticationService;
+    private final Function<AuthenticationToken, UserToken> tokenMapper;
 
+    SignUpOperation(UserRepository userRepo, 
+                    AuthenticationService.Iface authenticationService,
+                           Function<AuthenticationToken, UserToken> tokenMapper)
+    {
+        checkThat(userRepo, authenticationService, tokenMapper)
+            .are(notNull());
+        
+        this.userRepo = userRepo;
+        this.authenticationService = authenticationService;
+        this.tokenMapper = tokenMapper;
+    }
+    
+    
     @Override
     public SignUpResponse process(SignUpRequest request) throws TException
     {
-        checkNotNull(request);
-        
-        //Check validity of data
-        //Create User object
-        //Store in Repository
-        //Create and Acquire token from authentication service
-        
-        
-        SignUpResponse response = pojos(SignUpResponse.class).get();
-        return response;
-    }
+        checkThat(request)
+            .throwing(ex -> new InvalidArgumentException(ex.getMessage()))
+            .is(good());
 
+        //Create User object
+        String userId = UUID.randomUUID().toString();
+        
+        User user = new User()
+            .setBirthdate(request.birthDate)
+            .setEmail(request.email)
+            .setFirstName(request.firstName)
+            .setMiddleName(request.middleName)
+            .setLastName(request.lastName)
+            .setGithubProfile(request.githubProfile)
+            .setName(request.name)
+            .setProfileImage(request.profileImage)
+            .setRoles(Sets.createFrom(request.mainRole))
+            .setUserId(userId);
+
+        //Store in Repository
+        userRepo.saveUser(user);
+
+        //Create and Acquire token from authentication service
+        CreateTokenRequest authRequest = makeAuthenticationRequestToCreateToken(user);
+        AuthenticationToken token = tryToGetTokenFromAuthenticationService(authRequest);
+        
+        UserToken userToken = convertToUserToken(token);
+        
+        return new SignUpResponse()
+            .setUserId(userId)
+            .setUserToken(userToken);
+    }
+    
+    private AlchemyAssertion<SignUpRequest> good()
+    {
+        return request ->
+            {
+                checkThat(request)
+                    .usingMessage("request is null")
+                    .is(notNull());
+                
+                checkThat(request.firstName, request.lastName)
+                    .usingMessage("first and last name are required")
+                    .are(nonEmptyString());
+                
+                checkThat(request.mainRole)
+                    .usingMessage("Your main role is required")
+                    .is(notNull());
+                
+                if (request.isSetOrganizationId())
+                {
+                    checkThat(request.organizationId)
+                        .usingMessage("organization ID must be a valid UUID type")
+                        .is(validUUID());
+                }
+
+                //TODO: Add check on the email
+            };
+    }
+    
+    private CreateTokenRequest makeAuthenticationRequestToCreateToken(User user)
+    {
+        return new CreateTokenRequest()
+            .setDesiredTokenType(TokenType.USER)
+            .setOwnerId(user.userId)
+            .setOwnerName(user.name);
+    }
+    
+    private AuthenticationToken tryToGetTokenFromAuthenticationService(CreateTokenRequest authRequest) throws
+        OperationFailedException
+    {
+        CreateTokenResponse response;
+        try
+        {
+            response = authenticationService.createToken(authRequest);
+        }
+        catch (TException ex)
+        {
+            throw new OperationFailedException("Could not get token from the Authentication Service: " + ex.getMessage());
+        }
+        
+        checkThat(response.token)
+            .throwing(OperationFailedException.class)
+            .usingMessage("Auth Service returned invalid token")
+            .is(completeToken());
+        
+        return response.token;
+    }
+    
+    private UserToken convertToUserToken(AuthenticationToken token)
+    {
+        return tokenMapper.apply(token);
+    }
+    
 }
--- src/test/java/tech/aroma/banana/service/operations/SignUpOperationTest.java
+++ src/test/java/tech/aroma/banana/service/operations/SignUpOperationTest.java
@@ -16,20 +16,38 @@
 
 package tech.aroma.banana.service.operations;
 
+import java.util.function.Function;
+import org.apache.thrift.TException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import tech.aroma.banana.data.UserRepository;
+import tech.aroma.banana.thrift.User;
+import tech.aroma.banana.thrift.authentication.AuthenticationToken;
+import tech.aroma.banana.thrift.authentication.UserToken;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenRequest;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenResponse;
 import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
 import tech.aroma.banana.thrift.service.SignUpRequest;
 import tech.aroma.banana.thrift.service.SignUpResponse;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
 import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
 import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateString;
 import tech.sirwellington.alchemy.test.junit.runners.Repeat;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.UUID;
 
 /**
  *
@@ -37,17 +55,55 @@ import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThr
  */
 @Repeat(10)
 @RunWith(AlchemyTestRunner.class)
-public class SignUpOperationTest 
+public class SignUpOperationTest
 {
+
+    @Mock
+    private UserRepository userRepo;
+    
+    @Mock
+    private AuthenticationService.Iface authenticationService;
+    
+    @Mock
+    private Function<AuthenticationToken, UserToken> tokenMapper;
+
     @GeneratePojo
     private SignUpRequest request;
     
+    @GenerateString(UUID)
+    private String orgId;
+    
+    @GeneratePojo
+    private AuthenticationToken authToken;
+    
+    @GeneratePojo
+    private UserToken userToken;
+
     private SignUpOperation instance;
+    
+    @Captor
+    private ArgumentCaptor<User> userCaptor;
+    
+    @Captor
+    private ArgumentCaptor<CreateTokenRequest> requestCaptor;
+    
+    private CreateTokenResponse authResponse;
 
     @Before
-    public void setUp()
+    public void setUp() throws TException
     {
-        instance = new SignUpOperation();
+        instance = new SignUpOperation(userRepo, authenticationService, tokenMapper);
+        
+        verifyZeroInteractions(userRepo, authenticationService, tokenMapper);
+        
+        authResponse = new CreateTokenResponse(authToken);
+        when(authenticationService.createToken(any(CreateTokenRequest.class)))
+            .thenReturn(authResponse);
+        
+        when(tokenMapper.apply(authToken)).thenReturn(userToken);
+        
+        request.organizationId = orgId;
+        authToken.organizationId = orgId;
     }
 
     @Test
@@ -55,6 +111,7 @@ public class SignUpOperationTest
     {
         SignUpResponse response = instance.process(request);
         assertThat(response, notNullValue());
+        assertThat(response.userToken, is(userToken));
     }
 
     @DontRepeat
