50725222a7d5e303be969784c3265f0d03597af4
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-24 14:41:06 -0800

    ProvisionApplicationOperation: Adding legit implementation

--- src/main/java/tech/aroma/banana/service/operations/ProvisionApplicationOperation.java
+++ src/main/java/tech/aroma/banana/service/operations/ProvisionApplicationOperation.java
@@ -16,17 +16,42 @@
 
 package tech.aroma.banana.service.operations;
 
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.Function;
+import javax.inject.Inject;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sir.wellington.alchemy.collections.sets.Sets;
+import tech.aroma.banana.data.ApplicationRepository;
+import tech.aroma.banana.data.UserRepository;
+import tech.aroma.banana.thrift.Application;
+import tech.aroma.banana.thrift.LengthOfTime;
+import tech.aroma.banana.thrift.TimeUnit;
+import tech.aroma.banana.thrift.User;
+import tech.aroma.banana.thrift.authentication.ApplicationToken;
+import tech.aroma.banana.thrift.authentication.AuthenticationToken;
+import tech.aroma.banana.thrift.authentication.TokenType;
+import tech.aroma.banana.thrift.authentication.UserToken;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenRequest;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenResponse;
+import tech.aroma.banana.thrift.authentication.service.GetTokenInfoRequest;
+import tech.aroma.banana.thrift.authentication.service.GetTokenInfoResponse;
+import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.banana.thrift.exceptions.InvalidTokenException;
+import tech.aroma.banana.thrift.exceptions.OperationFailedException;
 import tech.aroma.banana.thrift.service.ProvisionApplicationRequest;
 import tech.aroma.banana.thrift.service.ProvisionApplicationResponse;
 import tech.sirwellington.alchemy.annotations.access.Internal;
+import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
-import static tech.aroma.banana.service.BananaAssertions.checkNotNull;
-import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
-import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
+import static java.time.Instant.now;
+import static tech.aroma.banana.data.assertions.AuthenticationAssertions.completeToken;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
 
 /**
  *
@@ -35,17 +60,155 @@ import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
 @Internal
 final class ProvisionApplicationOperation implements ThriftOperation<ProvisionApplicationRequest, ProvisionApplicationResponse>
 {
-
+    
     private final static Logger LOG = LoggerFactory.getLogger(ProvisionApplicationOperation.class);
-
+    
+    private final ApplicationRepository appRepo;
+    private final UserRepository userRepo;
+    private final AuthenticationService.Iface authenticationService;
+    private final Function<AuthenticationToken, ApplicationToken> appTokenMapper;
+    
+    @Inject
+    ProvisionApplicationOperation(ApplicationRepository appRepo, UserRepository userRepo,
+                                  AuthenticationService.Iface authenticationService,
+                                  Function<AuthenticationToken, ApplicationToken> appTokenMapper)
+    {
+        checkThat(appRepo, userRepo, authenticationService, appTokenMapper)
+            .are(notNull());
+        
+        this.appRepo = appRepo;
+        this.userRepo = userRepo;
+        this.authenticationService = authenticationService;
+        this.appTokenMapper = appTokenMapper;
+    }
+    
     @Override
     public ProvisionApplicationResponse process(ProvisionApplicationRequest request) throws TException
     {
-        checkNotNull(request);
-        
         LOG.info("Received request to provision an Application", request);
         
-        ProvisionApplicationResponse response = one(pojos(ProvisionApplicationResponse.class));
-        return response;
+        checkThat(request)
+            .throwing(ex -> new InvalidArgumentException(ex.getMessage()))
+            .is(good());
+        
+        AuthenticationToken authTokenForUser = getUserTokenFrom(request.token);
+        User user = userRepo.getUser(authTokenForUser.ownerId);
+        
+        Application app = createAppFrom(request, user);
+        appRepo.saveApplication(app);
+        
+        AuthenticationToken authTokenForApp = createAppTokenFor(app);
+        ApplicationToken appToken = appTokenMapper.apply(authTokenForApp);
+        
+        return new ProvisionApplicationResponse()
+            .setApplicationInfo(app)
+            .setApplicationToken(appToken);
+
+        //Get User from token
+        //Create the Application object
+        //Store application object
+        //Create Token for Application
+        //Return token
+    }
+    
+    private AlchemyAssertion<ProvisionApplicationRequest> good()
+    {
+        return request ->
+        {
+            checkThat(request)
+                .usingMessage("request is null")
+                .is(notNull());
+            
+            checkThat(request.token)
+                .usingMessage("request missing token")
+                .is(notNull());
+        };
+    }
+    
+    private AuthenticationToken getUserTokenFrom(UserToken token) throws InvalidTokenException, OperationFailedException
+    {
+        GetTokenInfoRequest request = new GetTokenInfoRequest()
+            .setTokenId(token.tokenId)
+            .setTokenType(TokenType.USER);
+        
+        GetTokenInfoResponse response;
+        
+        try
+        {
+            response = authenticationService.getTokenInfo(request);
+        }
+        catch (InvalidTokenException ex)
+        {
+            throw ex;
+        }
+        catch (Exception ex)
+        {
+            LOG.error("Failed to get token info from Authentication Service for {}", token, ex);
+            throw new OperationFailedException("Token invalid: " + ex.getMessage());
+        }
+        
+        checkThat(response.token)
+            .throwing(OperationFailedException.class)
+            .usingMessage("Auth service returned null response")
+            .is(notNull());
+        
+        return response.token;
+    }
+    
+    private Application createAppFrom(ProvisionApplicationRequest request, User user)
+    {
+        Set<String> owners = Sets.copyOf(request.owners);
+        //Creating user is automatically an Owner
+        owners.add(user.userId);
+        String appId = UUID.randomUUID().toString();
+        
+        return new Application()
+            .setApplicationId(appId)
+            .setName(request.applicationName)
+            .setApplicationDescription(request.applicationDescription)
+            .setOrganizationId(request.organizationId)
+            .setTier(request.tier)
+            .setProgrammingLanguage(request.programmingLanguage)
+            .setTimeOfProvisioning(now().toEpochMilli())
+            .setTotalMessagesSent(0L)
+            .setOwners(owners);
+    }
+    
+    private AuthenticationToken createAppTokenFor(Application app) throws OperationFailedException
+    {
+        LengthOfTime lifetime = new LengthOfTime()
+            .setUnit(TimeUnit.DAYS)
+            .setValue(180);
+        
+        CreateTokenRequest request = new CreateTokenRequest()
+            .setDesiredTokenType(TokenType.APPLICATION)
+            .setLifetime(lifetime)
+            .setOrganizationId(app.organizationId)
+            .setOwnerId(app.applicationId)
+            .setOwnerName(app.name);
+        
+        CreateTokenResponse response;
+        
+        try
+        {
+            response = authenticationService.createToken(request);
+        }
+        catch (TException ex)
+        {
+            LOG.error("Failed to create Token for Application: {}", app, ex);
+            throw new OperationFailedException("Could not create token for app: " + ex.getMessage());
+        }
+        
+        checkThat(response)
+            .throwing(OperationFailedException.class)
+            .usingMessage("Authentication Service returned null")
+            .is(notNull());
+        
+        checkThat(response.token)
+            .usingMessage("Auth Service returned incomplete token")
+            .throwing(OperationFailedException.class)
+            .is(completeToken());
+        
+        return response.token;
     }
 }
--- src/test/java/tech/aroma/banana/service/operations/ProvisionApplicationOperationTest.java
+++ src/test/java/tech/aroma/banana/service/operations/ProvisionApplicationOperationTest.java
@@ -16,18 +16,43 @@
 
 package tech.aroma.banana.service.operations;
 
+import java.util.function.Function;
+import org.apache.thrift.TException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import tech.aroma.banana.data.ApplicationRepository;
+import tech.aroma.banana.data.UserRepository;
+import tech.aroma.banana.thrift.Application;
+import tech.aroma.banana.thrift.User;
+import tech.aroma.banana.thrift.authentication.ApplicationToken;
+import tech.aroma.banana.thrift.authentication.AuthenticationToken;
+import tech.aroma.banana.thrift.authentication.TokenType;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenRequest;
+import tech.aroma.banana.thrift.authentication.service.CreateTokenResponse;
+import tech.aroma.banana.thrift.authentication.service.GetTokenInfoRequest;
+import tech.aroma.banana.thrift.authentication.service.GetTokenInfoResponse;
 import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.banana.thrift.exceptions.UserDoesNotExistException;
 import tech.aroma.banana.thrift.service.ProvisionApplicationRequest;
 import tech.aroma.banana.thrift.service.ProvisionApplicationResponse;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
 import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateString;
 import tech.sirwellington.alchemy.test.junit.runners.Repeat;
 
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
 
 /**
@@ -39,22 +64,108 @@ import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThr
 public class ProvisionApplicationOperationTest
 {
 
+    @Mock
+    private ApplicationRepository appRepo;
+
+    @Mock
+    private UserRepository userRepo;
+
+    @Mock
+    private AuthenticationService.Iface authenticationService;
+
+    @Mock
+    private Function<AuthenticationToken, ApplicationToken> appTokenMapper;
+
     @GeneratePojo
-    private ProvisionApplicationRequest request;
+    private AuthenticationToken authToken;
+    
+    @GeneratePojo
+    private ApplicationToken appToken;
+    
+    @Captor
+    private ArgumentCaptor<Application> captor;
+    
+    @Captor
+    private ArgumentCaptor<CreateTokenRequest> authRequestCaptor;
 
     private ProvisionApplicationOperation instance;
 
+    @GeneratePojo
+    private ProvisionApplicationRequest request;
+    
+    @GeneratePojo
+    private User user;
+    
+    @GenerateString
+    private String userId;
+    
     @Before
-    public void setUp()
+    public void setUp() throws TException
     {
-        instance = new ProvisionApplicationOperation();
+        instance = new ProvisionApplicationOperation(appRepo, userRepo, authenticationService, appTokenMapper);
+        
+        verifyZeroInteractions(appRepo, userRepo, authenticationService, appTokenMapper);
+        
+        setupData();
+        setupMocks();
+    }
+    
+    @DontRepeat
+    @Test
+    public void testConstructor()
+    {
+        assertThrows(() -> new ProvisionApplicationOperation(null, userRepo, authenticationService, appTokenMapper))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThrows(() -> new ProvisionApplicationOperation(appRepo, null, authenticationService, appTokenMapper))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThrows(() -> new ProvisionApplicationOperation(appRepo, userRepo, null, appTokenMapper))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThrows(() -> new ProvisionApplicationOperation(appRepo, userRepo, authenticationService, null))
+            .isInstanceOf(IllegalArgumentException.class);
+        
     }
 
     @Test
     public void testProcess() throws Exception
     {
         ProvisionApplicationResponse response = instance.process(request);
+        
+        verify(appRepo).saveApplication(captor.capture());
+        
+        Application savedApp = captor.getValue();
+        assertThat(savedApp, notNullValue());
+        assertThat(savedApp.name, is(request.applicationName));
+        assertThat(savedApp.tier, is(request.tier));
+        assertThat(savedApp.owners.contains(userId), is(true));
+        assertThat(savedApp.owners.containsAll(request.owners), is(true));
+        assertThat(savedApp.organizationId, is(request.organizationId));
+        assertThat(savedApp.applicationDescription, is(request.applicationDescription));
+        
         assertThat(response, notNullValue());
+        assertThat(response.applicationToken, is(appToken));
+        assertThat(response.applicationInfo, is(savedApp));
+        
+        verify(authenticationService).createToken(authRequestCaptor.capture());
+        
+        CreateTokenRequest authRequestMade = authRequestCaptor.getValue();
+        assertThat(authRequestMade, notNullValue());
+        assertThat(authRequestMade.organizationId, is(request.organizationId));
+        assertThat(authRequestMade.ownerId, is(savedApp.applicationId));
+        assertThat(authRequestMade.desiredTokenType, is(TokenType.APPLICATION));
+        assertThat(authRequestMade.ownerName, is(savedApp.name));
+    }
+    
+    @Test
+    public void testWhenUserDoesNotExist() throws Exception
+    {
+        when(userRepo.getUser(userId))
+            .thenThrow(UserDoesNotExistException.class);
+        
+        assertThrows(() -> instance.process(request))
+            .isInstanceOf(UserDoesNotExistException.class);
     }
 
     @Test
@@ -62,6 +173,30 @@ public class ProvisionApplicationOperationTest
     {
         assertThrows(() -> instance.process(null))
             .isInstanceOf(InvalidArgumentException.class);
+    }
+
+    private void setupData()
+    {
+        request.token.unsetUserId();
+        authToken.ownerId = userId;
+        user.userId = userId;
+    }
+
+    private void setupMocks() throws TException
+    {
+        GetTokenInfoRequest expectedAuthRequest = new GetTokenInfoRequest(request.token.tokenId, TokenType.USER);
+        
+        when(authenticationService.getTokenInfo(expectedAuthRequest))
+            .thenReturn(new GetTokenInfoResponse(authToken));
+        
+        when(userRepo.getUser(userId)).thenReturn(user);
+        
+        when(appTokenMapper.apply(authToken))
+            .thenReturn(appToken);
+            
+        
+        when(authenticationService.createToken(Mockito.any()))
+            .thenReturn(new CreateTokenResponse(authToken));
     }
 
 }
