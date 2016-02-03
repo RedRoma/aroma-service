fe8857aad665233bcb790fe641f927704efcb2e4
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-02-02 16:19:44 -0800

    Adding OverTheWireDecryptor

---
+++ src/main/java/tech/aroma/banana/service/operations/encryption/OverTheWireDecryptor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package tech.aroma.banana.service.operations.encryption;
+
+import com.google.inject.ImplementedBy;
+import org.apache.thrift.TException;
+import tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern;
+
+import static tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern.Role.INTERFACE;
+
+
+/**
+ * This interface is responsible for decrypting passwords and emails sent over the Wire
+ * from RedRoma Applications.
+ * Red Roma
+ * RedRoma
+ * 
+ * @author SirWellington
+ */
+@FunctionalInterface
+@StrategyPattern(role = INTERFACE)
+@ImplementedBy(OverTheWireDecryptorImpl.class)
+public interface OverTheWireDecryptor
+{
+
+    /**
+     * Takes an Encrypted String and decrypts it for consumption.
+     * 
+     * @param encrypredString The String to Decrypt
+     * @return 
+     * @throws org.apache.thrift.TException If the String could not be decrypted.
+     */
+    public String decrypt(String encrypredString) throws TException;
+    
+}
