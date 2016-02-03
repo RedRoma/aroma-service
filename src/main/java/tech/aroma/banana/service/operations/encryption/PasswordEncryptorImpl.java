66574fb26213b1b9093799937a33a4cce7e3d677
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-02-02 16:19:11 -0800

    Adding PasswordEncryptorImpl

---
+++ src/main/java/tech/aroma/banana/service/operations/encryption/PasswordEncryptorImpl.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.service.operations.encryption;
+
+import javax.inject.Inject;
+import org.apache.thrift.TException;
+import org.jasypt.digest.StringDigester;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.banana.thrift.exceptions.InvalidCredentialsException;
+import tech.aroma.banana.thrift.exceptions.OperationFailedException;
+import tech.sirwellington.alchemy.annotations.access.Internal;
+
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
+
+@Internal
+final class PasswordEncryptorImpl implements PasswordEncryptor
+{
+
+    private final static Logger LOG = LoggerFactory.getLogger(PasswordEncryptorImpl.class);
+    
+    private final StringDigester encryptor;
+    
+    @Inject
+    PasswordEncryptorImpl(StringDigester encryptor)
+    {
+        checkThat(encryptor).is(notNull());
+        
+        this.encryptor = encryptor;
+    }
+    
+    @Override
+    public String encryptPassword(String password) throws TException
+    {
+        checkThat(password)
+            .throwing(InvalidCredentialsException.class)
+            .is(nonEmptyString());
+        
+        try
+        {
+            return encryptor.digest(password);
+        }
+        catch (Exception ex)
+        {
+            LOG.error("Failed to digest and encrypt password", ex);
+            throw new InvalidCredentialsException("Could not digest password: " + ex.getMessage());
+        }
+    }
+    
+    @Override
+    public boolean match(String candidate, String existingDigestedPassword) throws TException
+    {
+        checkThat(candidate, existingDigestedPassword)
+            .throwing(InvalidCredentialsException.class)
+            .usingMessage("credentials cannot be empty")
+            .are(nonEmptyString());
+        
+        try
+        {
+            return encryptor.matches(candidate, existingDigestedPassword);
+        }
+        catch (Exception ex)
+        {
+            LOG.error("Failed to check if candidate matches existing digest", ex);
+            throw new OperationFailedException("Could not check credentials: " + ex.getMessage());
+        }
+    }
+    
+}
