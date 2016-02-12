e924313c5e16afb0f0d5ca8606d9a4324949cc14
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-02-12 10:34:55 -0800

    GetMyApplicationsOperation: Now returns real apps owned by a user

--- src/main/java/tech/aroma/banana/service/operations/GetMyApplicationsOperation.java
+++ src/main/java/tech/aroma/banana/service/operations/GetMyApplicationsOperation.java
@@ -1,35 +1,38 @@
- /*
-  * Copyright 2015 Aroma Tech.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package tech.aroma.banana.service.operations;
 
 import java.util.List;
+import javax.inject.Inject;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.aroma.banana.data.ApplicationRepository;
 import tech.aroma.banana.thrift.Application;
+import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
 import tech.aroma.banana.thrift.service.GetMyApplicationsRequest;
 import tech.aroma.banana.thrift.service.GetMyApplicationsResponse;
+import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
-import static tech.aroma.banana.service.BananaAssertions.checkNotNull;
-import static tech.aroma.banana.service.BananaGenerators.applications;
-import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
-import static tech.sirwellington.alchemy.generator.CollectionGenerators.listOf;
-import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
+import static tech.aroma.banana.data.assertions.RequestAssertions.validUserId;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
 
 /**
  *
@@ -40,25 +43,53 @@ final class GetMyApplicationsOperation implements ThriftOperation<GetMyApplicati
     
     private final static Logger LOG = LoggerFactory.getLogger(GetMyApplicationsOperation.class);
     
+    private final ApplicationRepository appRepo;
+    
+    @Inject
+    GetMyApplicationsOperation(ApplicationRepository appRepo)
+    {
+        checkThat(appRepo)
+            .is(notNull());
+        
+        this.appRepo = appRepo;
+    }
+    
     @Override
     public GetMyApplicationsResponse process(GetMyApplicationsRequest request) throws TException
     {
-        checkNotNull(request);
+        checkThat(request)
+            .throwing(ex -> new InvalidArgumentException(ex.getMessage()))
+            .is(good());
         
         LOG.debug("Received request to GetMyApplications {}", request);
         
-        GetMyApplicationsResponse response = new GetMyApplicationsResponse();
+        String userId = request.token.userId;
+        List<Application> apps = appRepo.getApplicationsOwnedBy(userId);
         
-        int count = one(integers(0, 40));
-        List<Application> fakeApplications = listOf(applications(), count);
-        response.setApplications(fakeApplications);
+        LOG.debug("Found {} applications owned by {}", apps.size(), userId);
         
-        LOG.info("Returning {} Applications for {}", fakeApplications.size(), request);
-        
-        return response;
-        
+        return new GetMyApplicationsResponse(apps);
     }
     
-    
+    private AlchemyAssertion<GetMyApplicationsRequest> good()
+    {
+        return request ->
+        {
+            checkThat(request)
+                .usingMessage("missing request")
+                .is(notNull());
+            
+            checkThat(request.token)
+                .usingMessage("request missing token")
+                .is(notNull());
+            
+            checkThat(request.token.userId)
+                .usingMessage("token missing userId")
+                .is(nonEmptyString());
+            
+            checkThat(request.token.userId)
+                .is(validUserId());
+        };
+    }
     
 }
