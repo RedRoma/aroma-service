e377fa9203cd178a11dd3873089a04428d9bfac7
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-02-26 15:44:41 -0800

    New Operation: GetApplicationsFollowedByOperation

---
+++ src/main/java/tech/aroma/service/operations/GetApplicationsFollowedByOperation.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016 RedRoma.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.service.operations;
+
+import java.util.List;
+import javax.inject.Inject;
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.data.ApplicationRepository;
+import tech.aroma.data.FollowerRepository;
+import tech.aroma.data.UserRepository;
+import tech.aroma.thrift.Application;
+import tech.aroma.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.thrift.service.GetApplicationsFollowedByRequest;
+import tech.aroma.thrift.service.GetApplicationsFollowedByResponse;
+import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
+import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
+
+import static tech.aroma.data.assertions.RequestAssertions.isNullOrEmpty;
+import static tech.aroma.data.assertions.RequestAssertions.validUserId;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+
+/**
+ *
+ * @author SirWellington
+ */
+final class GetApplicationsFollowedByOperation implements ThriftOperation<GetApplicationsFollowedByRequest, GetApplicationsFollowedByResponse>
+{
+
+    private final static Logger LOG = LoggerFactory.getLogger(GetApplicationsFollowedByOperation.class);
+    
+    private final ApplicationRepository appRepo;
+    private final FollowerRepository followerRepo;
+    private final UserRepository userRepo;
+    
+    @Inject
+    GetApplicationsFollowedByOperation(ApplicationRepository appRepo,
+                                       FollowerRepository followerRepo,
+                                       UserRepository userRepo)
+    {
+        checkThat(appRepo, followerRepo, userRepo)
+            .are(notNull());
+        
+        this.appRepo = appRepo;
+        this.followerRepo = followerRepo;
+        this.userRepo = userRepo;
+    }
+    
+    @Override
+    public GetApplicationsFollowedByResponse process(GetApplicationsFollowedByRequest request) throws TException
+    {
+        checkThat(request)
+            .throwing(ex -> new InvalidArgumentException(ex.getMessage()))
+            .is(good());
+        
+        String userId = request.userId;
+        
+        if (isNullOrEmpty(userId))
+        {
+            userId = request.token.userId;
+        }
+        
+        checkThat(userId)
+            .throwing(InvalidArgumentException.class)
+            .usingMessage("userID is invalid")
+            .is(validUserId());
+        
+        List<Application> apps = followerRepo.getApplicationsFollowedBy(userId);
+        
+        LOG.debug("Found {} apps followed by [{]]", apps.size(), userId);
+        
+        return new GetApplicationsFollowedByResponse(apps);
+    }
+    
+    private AlchemyAssertion<GetApplicationsFollowedByRequest> good()
+    {
+        return request ->
+        {
+            checkThat(request)
+                .usingMessage("request is null")
+                .is(notNull());
+            
+            checkThat(request.token)
+                .usingMessage("request missing token")
+                .is(notNull());
+            
+            checkThat(request.token.userId)
+                .usingMessage("userId in token is invalid")
+                .is(validUserId());
+            
+            if (request.isSetUserId())
+            {
+                checkThat(request.userId)
+                    .is(validUserId());
+            }
+        };
+    }
+    
+}
---
+++ src/test/java/tech/aroma/service/operations/GetApplicationsFollowedByOperationTest.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2016 RedRoma.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.service.operations;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import tech.aroma.data.ApplicationRepository;
+import tech.aroma.data.FollowerRepository;
+import tech.aroma.data.UserRepository;
+import tech.aroma.thrift.Application;
+import tech.aroma.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.thrift.service.GetApplicationsFollowedByRequest;
+import tech.aroma.thrift.service.GetApplicationsFollowedByResponse;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateList;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateString;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.ALPHABETIC;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.UUID;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(10)
+@RunWith(AlchemyTestRunner.class)
+public class GetApplicationsFollowedByOperationTest
+{
+
+    @Mock
+    private ApplicationRepository appRepo;
+
+    @Mock
+    private FollowerRepository followerRepo;
+
+    @Mock
+    private UserRepository userRepo;
+
+    private GetApplicationsFollowedByOperation instance;
+
+    @GenerateList(Application.class)
+    private List<Application> apps;
+
+    @GeneratePojo
+    private GetApplicationsFollowedByRequest request;
+
+    @GenerateString(UUID)
+    private String userId;
+
+    @GenerateString(UUID)
+    private String userIdOfCaller;
+
+    @GenerateString(ALPHABETIC)
+    private String badId;
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+        setupData();
+        setupMocks();
+
+        instance = new GetApplicationsFollowedByOperation(appRepo, followerRepo, userRepo);
+    }
+
+    private void setupData() throws Exception
+    {
+        request.token.userId = userIdOfCaller;
+        request.userId = userId;
+    }
+
+    private void setupMocks() throws Exception
+    {
+        when(followerRepo.getApplicationsFollowedBy(userId)).thenReturn(apps);
+        when(followerRepo.getApplicationsFollowedBy(userIdOfCaller)).thenReturn(apps);
+    }
+
+    @DontRepeat
+    @Test
+    public void testConstructor()
+    {
+        assertThrows(() -> new GetApplicationsFollowedByOperation(null, followerRepo, userRepo))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThrows(() -> new GetApplicationsFollowedByOperation(appRepo, null, userRepo))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThrows(() -> new GetApplicationsFollowedByOperation(appRepo, followerRepo, null))
+            .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    public void testProcess() throws Exception
+    {
+        GetApplicationsFollowedByResponse response = instance.process(request);
+        assertThat(response, notNullValue());
+        assertThat(response.applications, is(apps));
+
+        verify(followerRepo).getApplicationsFollowedBy(userId);
+        verify(followerRepo, never()).getApplicationsFollowedBy(userIdOfCaller);
+    }
+
+    @Test
+    public void testWhenNoUserIdSupplied() throws Exception
+    {
+        request.unsetUserId();
+
+        GetApplicationsFollowedByResponse response = instance.process(request);
+        assertThat(response, notNullValue());
+        assertThat(response.applications, is(apps));
+
+    }
+
+    @DontRepeat
+    @Test
+    public void testWithBadArgs() throws Exception
+    {
+        assertThrows(() -> instance.process(null))
+            .isInstanceOf(InvalidArgumentException.class);
+
+        //Empty Request
+        assertThrows(() -> instance.process(new GetApplicationsFollowedByRequest()))
+            .isInstanceOf(InvalidArgumentException.class);
+
+        //Request missing token
+        GetApplicationsFollowedByRequest requestMissingToken = new GetApplicationsFollowedByRequest(request);
+        requestMissingToken.unsetToken();
+        assertThrows(() -> instance.process(requestMissingToken))
+            .isInstanceOf(InvalidArgumentException.class);
+        
+        //Request with bad userId
+        GetApplicationsFollowedByRequest requestWithBadId = new GetApplicationsFollowedByRequest(request);
+        requestWithBadId.setUserId(badId);
+        assertThrows(() -> instance.process(requestWithBadId))
+            .isInstanceOf(InvalidArgumentException.class);
+ 
+        
+    }
+
+}
