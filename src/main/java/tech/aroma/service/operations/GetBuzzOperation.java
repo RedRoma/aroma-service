3e069efbca1af7cd78863815ca93f642921e6214
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-03-11 18:30:13 -0800

    GetBuzzOperation: Removing generated "Happenings" from Buzz

--- src/main/java/tech/aroma/service/operations/GetBuzzOperation.java
+++ src/main/java/tech/aroma/service/operations/GetBuzzOperation.java
@@ -26,23 +26,15 @@ import org.slf4j.LoggerFactory;
 import tech.aroma.data.ApplicationRepository;
 import tech.aroma.data.OrganizationRepository;
 import tech.aroma.data.UserRepository;
-import tech.aroma.service.AromaGenerators;
 import tech.aroma.thrift.Application;
 import tech.aroma.thrift.User;
-import tech.aroma.thrift.events.GeneralEvent;
-import tech.aroma.thrift.events.HealthCheckFailed;
 import tech.aroma.thrift.service.GetBuzzRequest;
 import tech.aroma.thrift.service.GetBuzzResponse;
-import tech.sirwellington.alchemy.generator.AlchemyGenerator;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
 import static tech.aroma.service.AromaAssertions.checkNotNull;
 import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
-import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
-import static tech.sirwellington.alchemy.generator.CollectionGenerators.listOf;
-import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
-import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
 
 /**
  *
@@ -74,45 +66,19 @@ final class GetBuzzOperation implements ThriftOperation<GetBuzzRequest, GetBuzzR
         
         List<Application> recentApps = appRepo.getRecentlyCreated();
         List<User> recentUsers = userRepo.getRecentlyCreatedUsers();
+        
         //Get recently created apps
         //Get the requester's orgs
         //Get recent users in those orgs
         //Get recent apps in those orgs
         //Return all that
-        
-        
-        GetBuzzResponse response = one(buzz());
-        response.setFreshApplications(recentApps);
-        response.setFreshUsers(recentUsers);
-        
+        GetBuzzResponse response = new GetBuzzResponse()
+            .setFreshApplications(recentApps)
+            .setFreshUsers(recentUsers);
+
         LOG.debug("Returning Buzz: {}", response);
         
         return response;
-    }
-
-    
-    private AlchemyGenerator<GetBuzzResponse> buzz()
-    {
-        return () ->
-        {
-          
-            GetBuzzResponse response = new GetBuzzResponse();
-            
-            AlchemyGenerator<HealthCheckFailed> healthChecks = pojos(HealthCheckFailed.class);
-            AlchemyGenerator<GeneralEvent> generalEvents = pojos(GeneralEvent.class);
-            
-            int numberOfUsers = one(integers(5, 22));
-            int numberOfFailedHealthChecks = one(integers(0, 6));
-            int numberOfGeneralHappenings = one(integers(5, 25));
-            
-            response.setFreshUsers(listOf(AromaGenerators.users(), numberOfUsers))
-                .setFailedHealthChecks(listOf(healthChecks, numberOfFailedHealthChecks))
-                .setGeneralEvents(listOf(generalEvents, numberOfGeneralHappenings));
-            
-            return response;
-            
-        };
-    
     }
     
 }
