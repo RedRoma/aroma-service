a4e22b09b98ac62428049e98d366a6c52b9a8108
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-03-25 22:40:31 -0700

    Adding ThumbnailCreator functionality

---
+++ src/main/java/tech/aroma/service/operations/thumbnails/ThumbnailCreatorImpl.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2016 RedRoma.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.aroma.service.operations.thumbnails;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.resizers.configurations.Antialiasing;
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.thrift.Dimension;
+import tech.aroma.thrift.Image;
+import tech.aroma.thrift.ImageType;
+import tech.aroma.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.thrift.exceptions.OperationFailedException;
+
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+import static tech.sirwellington.alchemy.arguments.assertions.BooleanAssertions.trueStatement;
+import static tech.sirwellington.alchemy.arguments.assertions.NumberAssertions.greaterThan;
+
+/**
+ *
+ * @author SirWellington
+ */
+final class ThumbnailCreatorImpl implements ThumbnailCreator
+{
+    private final static Logger LOG = LoggerFactory.getLogger(ThumbnailCreatorImpl.class);
+
+    @Override
+    public Image createThumbnail(Image originalImage, Dimension desiredSize) throws TException
+    {
+
+        checkThat(originalImage, desiredSize)
+            .throwing(InvalidArgumentException.class)
+            .usingMessage("missing arguments")
+            .are(notNull());
+        
+        checkThat(desiredSize.width, desiredSize.height)
+            .throwing(InvalidArgumentException.class)
+            .usingMessage("Thumbnail dimensions must be > 0")
+            .are(greaterThan(0));
+        
+        checkThat(originalImage.isSetData())
+            .throwing(InvalidArgumentException.class)
+            .usingMessage("original image must its data set")
+            .is(trueStatement());
+        
+        try (ByteArrayInputStream istream = new ByteArrayInputStream(originalImage.getData());
+             ByteArrayOutputStream ostream = new ByteArrayOutputStream();)
+        {
+            Thumbnails.of(istream)
+                .antialiasing(Antialiasing.ON)
+                .outputQuality(0.9)
+                .height(desiredSize.height)
+                .width(desiredSize.width)
+                .useExifOrientation(true)
+                .toOutputStream(ostream);
+
+            Image thumbnail = new Image()
+                .setData(ostream.toByteArray())
+                .setDimension(desiredSize)
+                .setImageType(ImageType.JPEG);
+
+            LOG.info("Successfully created thumbnail of Size {}", desiredSize);
+
+            return thumbnail;
+        }
+        catch (IOException ex)
+        {
+            LOG.error("Failed to create thumbnail for {} of size {}", originalImage, desiredSize, ex);
+            throw new OperationFailedException("Could not create thumbnail for " + originalImage + " of size " + desiredSize);
+        }
+    }
+
+}
---
+++ src/test/java/tech/aroma/service/operations/thumbnails/ThumbnailCreatorImplTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2016 RedRoma.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.service.operations.thumbnails;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tech.aroma.thrift.Dimension;
+import tech.aroma.thrift.Image;
+import tech.aroma.thrift.User;
+import tech.aroma.thrift.exceptions.InvalidArgumentException;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static tech.aroma.thrift.generators.UserGenerators.users;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(10)
+@RunWith(AlchemyTestRunner.class)
+public class ThumbnailCreatorImplTest 
+{
+
+    private User user;
+    
+    private Image image;
+    
+    @GeneratePojo
+    private Dimension thumbnailSize;
+    
+    private ThumbnailCreatorImpl instance;
+    
+    @Before
+    public void setUp() throws Exception
+    {
+        setupData();
+        setupMocks();
+        
+        instance = new ThumbnailCreatorImpl();
+    }
+
+    private void setupData() throws Exception
+    {
+        user = one(users());
+        image = user.profileImage;
+        
+    }
+
+    private void setupMocks() throws Exception
+    {
+        
+    }
+
+    @Test
+    public void testCreateThumbnail() throws Exception
+    {
+        Image thumbnail = instance.createThumbnail(image, thumbnailSize);
+        assertThat(thumbnail, notNullValue());
+        assertThat(thumbnail.data, notNullValue());
+        assertThat(thumbnail.dimension, is(thumbnailSize));
+    }
+    
+    @DontRepeat
+    @Test
+    public void testWithBadArgs()
+    {
+        assertThrows(() -> instance.createThumbnail(image, null))
+            .isInstanceOf(InvalidArgumentException.class);
+        
+        assertThrows(() -> instance.createThumbnail(null, thumbnailSize))
+            .isInstanceOf(InvalidArgumentException.class);
+        
+        Dimension badDimension = new Dimension().setHeight(0).setWidth(0);
+        assertThrows(() -> instance.createThumbnail(image, badDimension))
+            .isInstanceOf(InvalidArgumentException.class);
+        
+        Image badImage = new Image(image);
+        badImage.unsetData();
+        
+        assertThrows(() -> instance.createThumbnail(badImage, thumbnailSize))
+            .isInstanceOf(InvalidArgumentException.class);
+    }
+
+}
