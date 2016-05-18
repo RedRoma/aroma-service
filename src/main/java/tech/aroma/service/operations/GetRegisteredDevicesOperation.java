b5e15bb2e130a28146c05fff22d4c166a36e5f63
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-05-18 08:37:40 -0700

    GetRegisteredDevicesOperation: Updating for UserPreferencesRepository changes

--- src/main/java/tech/aroma/service/operations/GetRegisteredDevicesOperation.java
+++ src/main/java/tech/aroma/service/operations/GetRegisteredDevicesOperation.java
@@ -24,7 +24,6 @@ import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sir.wellington.alchemy.collections.lists.Lists;
-import tech.aroma.data.DeviceRepository;
 import tech.aroma.thrift.channels.MobileDevice;
 import tech.aroma.thrift.exceptions.InvalidArgumentException;
 import tech.aroma.thrift.service.GetRegisteredDevicesRequest;
@@ -33,8 +32,11 @@ import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
 import static tech.aroma.data.assertions.RequestAssertions.validUserId;
-import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+
+import tech.aroma.data.UserPreferencesRepository;
+
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 
 /**
  *
@@ -44,14 +46,14 @@ final class GetRegisteredDevicesOperation implements ThriftOperation<GetRegister
 {
     private final static Logger LOG = LoggerFactory.getLogger(GetRegisteredDevicesOperation.class);
     
-    private final DeviceRepository deviceRepo;
+    private final UserPreferencesRepository userPreferenceRepo;
 
     @Inject
-    GetRegisteredDevicesOperation(DeviceRepository deviceRepo)
+    GetRegisteredDevicesOperation(UserPreferencesRepository userPreferenceRepo)
     {
-        checkThat(deviceRepo).is(notNull());
+        checkThat(userPreferenceRepo).is(notNull());
         
-        this.deviceRepo = deviceRepo;
+        this.userPreferenceRepo = userPreferenceRepo;
     }
 
     @Override
@@ -63,7 +65,7 @@ final class GetRegisteredDevicesOperation implements ThriftOperation<GetRegister
         
         String userId = request.token.userId;
         
-        Set<MobileDevice> devices = deviceRepo.getMobileDevices(userId);
+        Set<MobileDevice> devices = userPreferenceRepo.getMobileDevices(userId);
         
         return new GetRegisteredDevicesResponse().setDevices(Lists.copy(devices));
     }
