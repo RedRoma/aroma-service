e68fdb4fd5d8227ff378ecbb71378547d4ae7566
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-05-21 17:29:08 -0700

    Adding concrete implementation for: RenewApplicationTokenOperation

--- src/main/java/tech/aroma/service/operations/RenewApplicationTokenOperation.java
+++ src/main/java/tech/aroma/service/operations/RenewApplicationTokenOperation.java
@@ -16,15 +16,36 @@
 
 package tech.aroma.service.operations;
 
+import java.util.function.Function;
+import javax.inject.Inject;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.aroma.data.ApplicationRepository;
+import tech.aroma.thrift.Application;
+import tech.aroma.thrift.LengthOfTime;
+import tech.aroma.thrift.TimeUnit;
+import tech.aroma.thrift.authentication.ApplicationToken;
+import tech.aroma.thrift.authentication.AuthenticationToken;
+import tech.aroma.thrift.authentication.TokenType;
+import tech.aroma.thrift.authentication.service.AuthenticationService;
+import tech.aroma.thrift.authentication.service.CreateTokenRequest;
+import tech.aroma.thrift.authentication.service.CreateTokenResponse;
+import tech.aroma.thrift.authentication.service.InvalidateTokenRequest;
+import tech.aroma.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.thrift.exceptions.OperationFailedException;
+import tech.aroma.thrift.exceptions.UnauthorizedException;
 import tech.aroma.thrift.service.RenewApplicationTokenRequest;
 import tech.aroma.thrift.service.RenewApplicationTokenResponse;
+import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
-import static tech.aroma.service.AromaAssertions.checkNotNull;
-import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
+import static tech.aroma.data.assertions.AuthenticationAssertions.completeToken;
+import static tech.aroma.data.assertions.RequestAssertions.validApplicationId;
+import static tech.aroma.data.assertions.RequestAssertions.validUserId;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+import static tech.sirwellington.alchemy.arguments.assertions.CollectionAssertions.elementInCollection;
 
 /**
  *
@@ -34,18 +55,120 @@ final class RenewApplicationTokenOperation implements ThriftOperation<RenewAppli
 {
 
     private final static Logger LOG = LoggerFactory.getLogger(RenewApplicationTokenOperation.class);
+    private final static LengthOfTime DEFAULT_TOKEN_LIFETIME = new LengthOfTime(TimeUnit.DAYS, 180);
+
+    private final AuthenticationService.Iface authenticationService;
+    private final ApplicationRepository appRepo;
+    private final Function<AuthenticationToken, ApplicationToken> tokenMapper;
+
+    @Inject
+    RenewApplicationTokenOperation(AuthenticationService.Iface authenticationService, 
+                                   ApplicationRepository appRepo,
+                                   Function<AuthenticationToken, ApplicationToken> tokenMapper)
+    {
+        checkThat(authenticationService, appRepo, tokenMapper).is(notNull());
+
+        this.authenticationService = authenticationService;
+        this.appRepo = appRepo;
+        this.tokenMapper = tokenMapper;
+    }
 
     @Override
     public RenewApplicationTokenResponse process(RenewApplicationTokenRequest request) throws TException
     {
-        checkNotNull(request);
-        //Check user is authorized for this action
-        //Check that we have been paid
-        //Delete the old token
-        //Create a new one
-        //Issue and return the new token
+        checkThat(request)
+            .throwing(ex -> new InvalidArgumentException(ex.getMessage()))
+            .is(good());
 
-        return pojos(RenewApplicationTokenResponse.class).get();
+        String appId = request.applicationId;
+        Application app = appRepo.getById(appId);
+
+        String userId = request.token.userId;
+        checkThat(userId)
+            .throwing(UnauthorizedException.class)
+            .is(elementInCollection(app.owners));
+
+        deleteTokensFor(appId);
+        
+        ApplicationToken newToken = createNewTokenFor(app);
+        
+        app.setTimeOfTokenExpiration(newToken.timeOfExpiration);
+        appRepo.saveApplication(app);
+        
+        return new RenewApplicationTokenResponse().setServiceToken(newToken);
+    }
+
+    private AlchemyAssertion<RenewApplicationTokenRequest> good()
+    {
+        return request ->
+        {
+            checkThat(request).is(notNull());
+            
+            checkThat(request.applicationId)
+                .is(validApplicationId());
+            
+            checkThat(request.token)
+                .usingMessage("missing user token")
+                .is(notNull());
+            
+            checkThat(request.token.userId)
+                .is(validUserId());
+        };
+    }
+
+    private void deleteTokensFor(String appId) throws OperationFailedException
+    {
+        InvalidateTokenRequest request = new InvalidateTokenRequest()
+            .setBelongingTo(appId);
+
+        try
+        {
+            authenticationService.invalidateToken(request);
+        }
+        catch (TException ex)
+        {
+            LOG.error("Failed to delete all tokens for App {}", appId, ex);
+            throw new OperationFailedException("Could not delete existing tokens for App: " + ex.getMessage());
+        }
+        
+        LOG.debug("Successfully deleted tokens for App {}", appId);
+    }
+
+    private ApplicationToken createNewTokenFor(Application app) throws InvalidArgumentException, OperationFailedException
+    {
+        CreateTokenRequest request = createRequestToCreateTokenFor(app);
+
+        CreateTokenResponse response;
+        try
+        {
+            response = authenticationService.createToken(request);
+        }
+        catch (TException ex)
+        {
+            LOG.error("Failed to create token for App {}", app, ex);
+            throw new OperationFailedException("Could not create a new Token for App: " + ex.getMessage());
+        }
+
+        AuthenticationToken authToken = response.token;
+
+        checkThat(authToken)
+            .throwing(OperationFailedException.class)
+            .usingMessage("Authentication Service returned incomplete Token")
+            .is(completeToken());
+
+        return tokenMapper.apply(authToken);
+    }
+
+    private CreateTokenRequest createRequestToCreateTokenFor(Application app) throws InvalidArgumentException
+    {
+        LengthOfTime timeToLive = DEFAULT_TOKEN_LIFETIME;
+
+        return new CreateTokenRequest()
+            .setOwnerId(app.applicationId)
+            .setDesiredTokenType(TokenType.APPLICATION)
+            .setLifetime(timeToLive)
+            .setOrganizationId(app.organizationId)
+            .setOwnerName(app.name);
     }
 
 }
--- src/test/java/tech/aroma/service/operations/RenewApplicationTokenOperationTest.java
+++ src/test/java/tech/aroma/service/operations/RenewApplicationTokenOperationTest.java
@@ -16,46 +16,114 @@
 
 package tech.aroma.service.operations;
 
+import java.time.Instant;
+import java.util.function.Function;
+import org.apache.thrift.TException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import tech.aroma.data.ApplicationRepository;
+import tech.aroma.thrift.Application;
+import tech.aroma.thrift.authentication.ApplicationToken;
+import tech.aroma.thrift.authentication.AuthenticationToken;
+import tech.aroma.thrift.authentication.service.AuthenticationService;
+import tech.aroma.thrift.authentication.service.CreateTokenResponse;
+import tech.aroma.thrift.authentication.service.InvalidateTokenRequest;
+import tech.aroma.thrift.authentication.service.InvalidateTokenResponse;
 import tech.aroma.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.thrift.exceptions.UnauthorizedException;
 import tech.aroma.thrift.service.RenewApplicationTokenRequest;
 import tech.aroma.thrift.service.RenewApplicationTokenResponse;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
 import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
 import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateString;
 import tech.sirwellington.alchemy.test.junit.runners.Repeat;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static tech.aroma.thrift.generators.ApplicationGenerators.applications;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.TimeGenerators.futureInstants;
 import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.HEXADECIMAL;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.UUID;
 
 /**
  *
  * @author SirWellington
  */
-@Repeat(10)
+@Repeat(100)
 @RunWith(AlchemyTestRunner.class)
 public class RenewApplicationTokenOperationTest
 {
+    @Mock
+    private AuthenticationService.Iface authenticationService;
+
+    @Mock
+    private ApplicationRepository appRepo;
+
+    @Mock
+    private Function<AuthenticationToken, ApplicationToken> tokenMapper;
 
     @GeneratePojo
     private RenewApplicationTokenRequest request;
 
+    private Application app;
+
+    @GeneratePojo
+    private ApplicationToken appToken;
+
+    @GeneratePojo
+    private AuthenticationToken authToken;
+
+    @GenerateString(UUID)
+    private String userId;
+
+    @GenerateString(UUID)
+    private String appId;
+
+    @GenerateString(HEXADECIMAL)
+    private String tokenId;
+
     private RenewApplicationTokenOperation instance;
 
     @Before
-    public void setUp()
+    public void setUp() throws TException
     {
-        instance = new RenewApplicationTokenOperation();
+        instance = new RenewApplicationTokenOperation(authenticationService, appRepo, tokenMapper);
+        verifyZeroInteractions(authenticationService, appRepo, tokenMapper);
+
+        setupData();
+        setupMocks();
     }
 
     @Test
     public void testProcess() throws Exception
     {
+        long originalExpiration = app.timeOfTokenExpiration;
+        
         RenewApplicationTokenResponse response = instance.process(request);
         assertThat(response, notNullValue());
+        assertThat(response.serviceToken, is(appToken));
+        assertThat(app.timeOfTokenExpiration, not(originalExpiration));
+        assertThat(app.timeOfTokenExpiration, is(appToken.timeOfExpiration));
+        
+        verify(appRepo).saveApplication(app);
+    }
+
+    @Test
+    public void testProcessWhenNotAuthorized() throws Exception
+    {
+        app.owners.remove(userId);
+
+        assertThrows(() -> instance.process(request))
+            .isInstanceOf(UnauthorizedException.class);
     }
 
     @DontRepeat
@@ -64,6 +132,53 @@ public class RenewApplicationTokenOperationTest
     {
         assertThrows(() -> instance.process(null))
             .isInstanceOf(InvalidArgumentException.class);
+
+        RenewApplicationTokenRequest emptyRequest = new RenewApplicationTokenRequest();
+        assertThrows(() -> instance.process(emptyRequest))
+            .isInstanceOf(InvalidArgumentException.class);
+
+        RenewApplicationTokenRequest requestWithoutToken = new RenewApplicationTokenRequest(request);
+        requestWithoutToken.unsetToken();
+        assertThrows(() -> instance.process(requestWithoutToken))
+            .isInstanceOf(InvalidArgumentException.class);
+
+        RenewApplicationTokenRequest requestWithoutAppId = new RenewApplicationTokenRequest(request);
+        requestWithoutAppId.unsetApplicationId();
+        assertThrows(() -> instance.process(requestWithoutAppId))
+            .isInstanceOf(InvalidArgumentException.class);
+
+    }
+
+    private void setupMocks() throws TException
+    {
+        when(tokenMapper.apply(authToken)).thenReturn(appToken);
+        when(appRepo.getById(appId)).thenReturn(app);
+
+        InvalidateTokenRequest invalidateRequest = new InvalidateTokenRequest()
+            .setBelongingTo(appId);
+        when(authenticationService.invalidateToken(invalidateRequest))
+            .thenReturn(new InvalidateTokenResponse());
+
+        when(authenticationService.createToken(Mockito.any()))
+            .thenReturn(new CreateTokenResponse(authToken));
+    }
+
+    private void setupData()
+    {
+        app = one(applications());
+        app.applicationId = appId;
+        
+        request.token.userId = userId;
+        request.applicationId = appId;
+
+        appToken.tokenId = tokenId;
+        authToken.tokenId = tokenId;
+
+        app.owners.add(userId);
+
+        Instant tokenExpiration = one(futureInstants());
+        authToken.timeOfExpiration = tokenExpiration.toEpochMilli();
+        appToken.timeOfExpiration = authToken.timeOfExpiration;
     }
 
 }
